b1be77c66990fb375f62ee148cb21afe1a842c77
Anton Stubenbord <anton.stubenbord@protonmail.com>
2022-10-19 11:42:59 +0200

    Fixed formatting

--- lib/src/fields/form_builder_color_picker.dart
+++ lib/src/fields/form_builder_color_picker.dart
@@ -177,7 +177,8 @@ class FormBuilderColorPickerField extends FormBuilderField<Color> {
         );
 
   @override
-  FormBuilderColorPickerFieldState createState() => FormBuilderColorPickerFieldState();
+  FormBuilderColorPickerFieldState createState() =>
+      FormBuilderColorPickerFieldState();
 }
 
 class FormBuilderColorPickerFieldState
